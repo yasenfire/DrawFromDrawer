600ce1fe790929556768c116d00f6deb499776d5
Alexey Fedotov <yasenfire@gmail.com>
2023-12-10 02:58:31 +0300

    Support of liquids and powders

--- Implementation.cs
+++ Implementation.cs
@@ -59,6 +59,26 @@ namespace DrawFromDrawer
                     maximums.Add(maximum);
 				}
 
+				foreach (BlueprintData.RequiredLiquid rl in bd.m_RequiredLiquid)
+				{
+					float inInventory = playerInventory.GetTotalLiquidVolume(rl.m_Liquid);
+					float inContainer = associatedContainerA.GetLiquidItemAmount(rl.m_Liquid);
+					float inContainerB = associatedContainerB is not null ? associatedContainerB.GetLiquidItemAmount(rl.m_Liquid) : 0;
+					float total = inInventory + inContainer + inContainerB;
+					int maximum = (int)Math.Floor(total / rl.m_VolumeInLitres);
+					maximums.Add(maximum);
+				}
+
+				foreach (BlueprintData.RequiredPowder rp in bd.m_RequiredPowder)
+				{
+					float inInventory = playerInventory.GetTotalPowderWeight(rp.m_Powder);
+					float inContainer = associatedContainerA.GetPowderItemAmount(rp.m_Powder);
+					float inContainerB = associatedContainerB is not null ? associatedContainerB.GetPowderItemAmount(rp.m_Powder) : 0;
+					float total = inInventory + inContainer + inContainerB;
+					int maximum = (int)Math.Floor(total / rp.m_QuantityInKilograms);
+					maximums.Add(maximum);
+				}
+
 				crqs.m_Maximum = maximums.Min();
             }
 		}
@@ -188,5 +208,155 @@ namespace DrawFromDrawer
 				__instance.ApplyTints(total >= requiredTotal, panel.m_RequirementsNotMetTint);
             }
 		}
-	}
+
+        [HarmonyPatch(typeof(CraftingRequirementMaterial), nameof(CraftingRequirementMaterial.EnableForLiquid))]
+        internal class CraftingRequirementMaterialEnableForLiquidPatch
+        {
+            static void Postfix(CraftingRequirementMaterial __instance, Panel_Crafting panel, BlueprintData bp, int liquidIndex, int quantity)
+            {
+                if (associatedContainerA is null) return;
+                Inventory playerInventory = GameObject.Find("SCRIPT_PlayerSystems").GetComponent<Inventory>();
+                BlueprintData.RequiredLiquid rl = bp.m_RequiredLiquid[liquidIndex];
+                float inInventory = playerInventory.GetTotalLiquidVolume(rl.m_Liquid);
+                float inContainer = associatedContainerA.GetLiquidItemAmount(rl.m_Liquid);
+                float inContainerB = associatedContainerB is not null ? associatedContainerB.GetLiquidItemAmount(rl.m_Liquid) : 0;
+                float total = inInventory + inContainer + inContainerB;
+                float requiredTotal = rl.m_VolumeInLitres * quantity;
+
+                string tail = __instance.m_Display.mText.Split("/")[1];
+                __instance.m_Display.mText = total.ToString("0.00") + "/" + tail;
+                __instance.m_Display.MarkAsChanged();
+
+                __instance.ApplyTints(total >= requiredTotal, panel.m_RequirementsNotMetTint);
+            }
+        }
+
+        [HarmonyPatch(typeof(CraftingRequirementMaterial), nameof(CraftingRequirementMaterial.EnableForPowder))]
+        internal class CraftingRequirementMaterialEnableForPowderPatch
+        {
+            static void Postfix(CraftingRequirementMaterial __instance, Panel_Crafting panel, BlueprintData bp, int powderIndex, int quantity)
+            {
+                if (associatedContainerA is null) return;
+                Inventory playerInventory = GameObject.Find("SCRIPT_PlayerSystems").GetComponent<Inventory>();
+                BlueprintData.RequiredPowder rp = bp.m_RequiredPowder[powderIndex];
+                float inInventory = playerInventory.GetTotalPowderWeight(rp.m_Powder);
+                float inContainer = associatedContainerA.GetPowderItemAmount(rp.m_Powder);
+                float inContainerB = associatedContainerB is not null ? associatedContainerB.GetPowderItemAmount(rp.m_Powder) : 0;
+                float total = inInventory + inContainer + inContainerB;
+                float requiredTotal = rp.m_QuantityInKilograms * quantity;
+
+                string tail = __instance.m_Display.mText.Split("/")[1];
+                __instance.m_Display.mText = total.ToString("0.00") + "/" + tail;
+                __instance.m_Display.MarkAsChanged();
+
+                __instance.ApplyTints(total >= requiredTotal, panel.m_RequirementsNotMetTint);
+            }
+        }
+
+		[HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.GetTotalLiters))]
+		internal class PlayerManagerGetTotalLitersPatch
+		{
+			static float Postfix(float result, PlayerManager __instance, LiquidType liquidType)
+			{
+				if (associatedContainerA is null) return result;
+
+                Inventory playerInventory = GameObject.Find("SCRIPT_PlayerSystems").GetComponent<Inventory>();
+                float inInventory = playerInventory.GetTotalLiquidVolume(liquidType);
+                float inContainer = associatedContainerA.GetLiquidItemAmount(liquidType);
+                float inContainerB = associatedContainerB is not null ? associatedContainerB.GetLiquidItemAmount(liquidType) : 0;
+                float total = inInventory + inContainer + inContainerB;
+				return total;
+            }
+		}
+
+        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.GetTotalPowderWeight))]
+        internal class PlayerManagerGetTotalPowderWeightPatch
+        {
+            static float Postfix(float result, PlayerManager __instance, PowderType type)
+            {
+                if (associatedContainerA is null) return result;
+
+                Inventory playerInventory = GameObject.Find("SCRIPT_PlayerSystems").GetComponent<Inventory>();
+                float inInventory = playerInventory.GetTotalPowderWeight(type);
+                float inContainer = associatedContainerA.GetPowderItemAmount(type);
+                float inContainerB = associatedContainerB is not null ? associatedContainerB.GetPowderItemAmount(type) : 0;
+                float total = inInventory + inContainer + inContainerB;
+                return total;
+            }
+        }
+
+		[HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DeductLiquidFromInventory))]
+		internal class PlayerManagerDeductLiquidFromInventoryPatch
+		{
+			static void Prefix(PlayerManager __instance, float literDeduction, LiquidType liquidType)
+			{
+				if (associatedContainerA is null) return;
+
+				Inventory playerInventory = __instance.gameObject.GetComponent<Inventory>();
+				float inInventory = playerInventory.GetTotalLiquidVolume(liquidType);
+				float missing = literDeduction - inInventory;
+				if (missing <= 0) return;
+
+				foreach (GearItemObject gio in associatedContainerA.m_Items)
+				{
+					if (missing <= 0) return;
+					GearItem gi = gio.m_GearItem;
+					if (gi.m_LiquidItem is null || gi.m_LiquidItem.LiquidType != liquidType) continue;
+					float numToRemove = Math.Min(missing, gi.m_LiquidItem.m_LiquidLiters);
+					gi.m_LiquidItem.m_LiquidLiters -= numToRemove;
+					missing -= numToRemove;
+				}
+
+				if (associatedContainerB is null) return;
+
+				foreach (GearItemObject gio in associatedContainerB.m_Items)
+				{
+					if (missing <= 0) return;
+					GearItem gi = gio.m_GearItem;
+					if (gi.m_LiquidItem is null || gi.m_LiquidItem.LiquidType != liquidType) continue;
+					float numToRemove = Math.Min(missing, gi.m_LiquidItem.m_LiquidLiters);
+					gi.m_LiquidItem.m_LiquidLiters -= numToRemove;
+					missing -= numToRemove;
+				}
+			}
+		}
+
+        [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.DeductPowderFromInventory))]
+        internal class PlayerManagerDeductPowderFromInventoryPatch
+        {
+            static void Prefix(PlayerManager __instance, float deduction, PowderType type)
+            {
+                if (associatedContainerA is null) return;
+
+                Inventory playerInventory = __instance.gameObject.GetComponent<Inventory>();
+                float inInventory = playerInventory.GetTotalPowderWeight(type);
+                float missing = deduction - inInventory;
+                if (missing <= 0) return;
+
+                foreach (GearItemObject gio in associatedContainerA.m_Items)
+                {
+                    if (missing <= 0) return;
+                    GearItem gi = gio.m_GearItem;
+                    if (gi.m_PowderItem is null || gi.m_PowderItem.m_Type != type) continue;
+                    float numToRemove = Math.Min(missing, gi.m_PowderItem.m_WeightKG);
+                    gi.m_PowderItem.m_WeightKG -= numToRemove;
+					if (gi.m_PowderItem.m_WeightKG <= 0) associatedContainerA.DestroyGear(gi);
+                    missing -= numToRemove;
+                }
+
+                if (associatedContainerB is null) return;
+
+                foreach (GearItemObject gio in associatedContainerB.m_Items)
+                {
+                    if (missing <= 0) return;
+                    GearItem gi = gio.m_GearItem;
+                    if (gi.m_PowderItem is null || gi.m_PowderItem.m_Type != type) continue;
+                    float numToRemove = Math.Min(missing, gi.m_PowderItem.m_WeightKG);
+                    gi.m_PowderItem.m_WeightKG -= numToRemove;
+                    if (gi.m_PowderItem.m_WeightKG <= 0) associatedContainerB.DestroyGear(gi);
+                    missing -= numToRemove;
+                }
+            }
+        }
+    }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 //Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
 //Mods typically use 3 numbers. For example: 1.2.1
 //The mod version need specified in three places.
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
-[assembly: MelonInfo(typeof(DrawFromDrawer.DrawFromDrawer), "DrawFromDrawer", "0.6.0", "yasenfire", null)]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: MelonInfo(typeof(DrawFromDrawer.DrawFromDrawer), "DrawFromDrawer", "0.7.0", "yasenfire", null)]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
